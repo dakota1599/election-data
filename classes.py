f422ab10ed596c2124e9ef4f531e02933e31ea91
Dakota Shapiro <dakotashapiro15@gmail.com>
2023-10-15 23:09:24 -0400

    Documents

---
+++ classes.py
@@ -0,0 +1,101 @@
+
+class COLORS:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+
+class Candidate:
+
+    def __init__(self, id, name, party, votes, win, od):
+        self.id = id
+        self.name = name
+        self.party = party
+        self.votes: int = votes
+        self.win = win
+        self.officeDescription = od
+
+    def __str__(self):
+        return "{} ({}) - {} - {} votes, Elected?: {}\n".format(self.name, self.party, self.officeDescription, self.votes, self.win)
+
+
+class County:
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.candidates = []
+
+    def addCandidate(self, c: Candidate):
+        self.candidates.append(c)
+
+    def __str__(self):
+        res = "\n{}\n----------\n".format(self.name)
+        for c in self.candidates:
+            res += c.__str__()
+
+        return res
+    
+    def sort(self):
+        self.candidates.sort(key=lambda x: x.votes, reverse=True)
+
+    def topTwo(self, pos):
+        l = [x for x in self.candidates if x.officeDescription == pos]
+        l.sort
+        res = "\n{}\n----------\n".format(self.name)
+        if len(l) >= 2:
+            lColor = ""
+            rColor = ""
+
+            if l[0].votes > l[1].votes:
+                lColor = COLORS.GREEN
+                rColor = COLORS.RED
+            else:
+                lColor = COLORS.RED
+                rColor = COLORS.GREEN
+
+            res += lColor + l[0].__str__()
+            res += rColor + l[1].__str__()
+            res += COLORS.WHITE + "Difference: {}\n".format(abs(int(l[0].votes) - int(l[1].votes)))
+        elif len(l) == 1:
+            res += COLORS.WHITE + l[0].__str__()
+        else:
+            res = ""
+        
+        res += COLORS.WHITE
+        
+        return res
+
+
+
+
+class Election:
+
+    def __init__(self):
+        self.counties = []
+
+    def find(self, id):
+        for ct in self.counties:
+            if ct.id == id:
+                return ct
+            
+        return None
+    
+    def addCounty(self, county: County):
+        self.counties.append(county)
+    
+    def __str__(self):
+        res = ""
+
+        for ct in self.counties:
+            res += ct.__str__()
+
+        return res
+    
+    def topTwo(self, name):
+        res = ""
+
+        for ct in self.counties:
+            res += ct.topTwo(name)
+
+        return res
+    
---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from classes import *
+def main():
+    fname = sys.argv[1]
+    pos = sys.argv[2]
+
+    f = open(fname, 'r')
+
+    election = Election()
+
+    for line in f:
+        w = line.split(',')
+        county = election.find(w[4])
+        setCounty = False
+        if (county == None):
+            county = County(w[4], w[5])
+            setCounty = True
+
+        win = False
+        if w[18] == "E\n" or w[18] == "N\n":
+            win = True
+
+        candidate = Candidate(w[10], w[11] + ', ' + w[12], w[8], w[15], win, w[6])
+
+        county.addCandidate(candidate)
+
+        if(setCounty):
+            election.addCounty(county)
+
+    
+    print(election.topTwo(pos))
+
+    f.close()
+
+
+
+if (__name__ == "__main__"):
+    main()
